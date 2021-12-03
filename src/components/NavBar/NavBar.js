2f7f28315bca9dd6e2ac52e025b26f386bf2739f
Ariloco27 <arielcaroprese96@gmail.com>
2021-12-03 01:49:08 -0300

    [FIX] Se arregló un componente Link

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,15 @@
 import './NavBar.css';
 import CartWidget from "../CartWidget/CartWidget"
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
      <nav className="navBar">
-        <div className="logoHeader">
-            <p>Pantera</p>
-        </div>
+        <Link to="/" className="NavLink">
+            <div className="logoHeader">
+                <p>Pantera</p>
+            </div>
+        </Link> 
         <NavLink to="/" className="NavLink">
             Home
         </NavLink> 
