a43fd29e24a0c6ea5e0702635c343fb497cf1aba
Ariloco27 <arielcaroprese96@gmail.com>
2021-12-07 23:04:07 -0300

    [ADD] Agregado de ItemCount en ItemDetail

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import './ItemDetail.css'
+import ItemCount from '../ItemCount/ItemCount'
 
 const ItemDetail = ({ item }) => {
+    const addToCart = (counter) => {
+        let prod;
+        counter > 1 ? prod = 'productos' : prod = 'producto';
+        alert(`Agregaste ${counter} ${prod} al carrito.`)
+    }
     return (
         <div className="ItemDetailContainer">
             <img src={item.thumbnail} alt={item.title} className="imgItemDetail"></img>
@@ -9,7 +15,7 @@ const ItemDetail = ({ item }) => {
                 <h1>{item.title}</h1>
                 <p>ID: {item.id}</p>
                 <p>Stock: {item.available_quantity}</p>
-                <p>Descripción:</p>
+                <ItemCount stock={item.available_quantity} initial={1} onAdd={addToCart}/>
             </div>
         </div>
     )
